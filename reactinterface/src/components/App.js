3a74d1bcf710a47b48909cea3da923f513fe4dcd
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2019-04-20 00:21:39 -0400

    Update components & CSS

--- reactinterface/src/components/App.js
+++ reactinterface/src/components/App.js
@@ -31,15 +31,20 @@ class App extends Component {
 
 
   render() {
+
+    
+
+
     return (
       <main className="page bg-white" id="petratings">
         <div className="container">
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
+                
                 <AddAppointments />
                 <SearchAppointments />
-                <ListAppointments />
+                <ListAppointments appointments={this.state.myAppointments} />
               </div>
             </div>
           </div>
